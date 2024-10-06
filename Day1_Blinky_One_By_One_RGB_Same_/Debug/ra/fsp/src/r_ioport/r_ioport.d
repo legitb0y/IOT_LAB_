eddaf25df976b423bf66100153677054ed784fb7
xenwithu <lvd.levandat@gmail.com>
2024-10-06 15:10:31 +0700

    feat:all lab

---
+++ Day1_Blinky_One_By_One_RGB_Same_/Debug/ra/fsp/src/r_ioport/r_ioport.d
@@ -0,0 +1,101 @@
+ra/fsp/src/r_ioport/r_ioport.o: ../ra/fsp/src/r_ioport/r_ioport.c \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/bsp_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/fsp_common_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/fsp_version.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_gen/bsp_clock_cfg.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_mcu_info.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_elc.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_feature.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_feature_gen.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board_init.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board_leds.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board_ethernet_phy.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_exceptions.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_gen/vector_data.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/renesas.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_gcc.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/R7FA6M5BH.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/core_cm33.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_version.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/m-profile/armv8m_mpu.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/system.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/system.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_common.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../inc/api/fsp_common_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_compiler_support.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_tfu.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_sdram.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_register_protection.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_irq.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_io.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_group_irq.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_clocks.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_module_stop.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_security.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../inc/fsp_features.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/fsp_common_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../inc/../../fsp/src/bsp/mcu/all/bsp_compiler_support.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_delay.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_mcu_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/instances/r_ioport.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/r_ioport_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/bsp_api.h \
+ /home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/r_ioport_cfg.h
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/bsp_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/fsp_common_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/fsp_version.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_gen/bsp_clock_cfg.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_mcu_info.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_elc.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_feature.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/fsp/src/bsp/mcu/ra6m5/bsp_feature_gen.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board_init.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board_leds.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/bsp/../../../ra/board/ra6m5_ck/board_ethernet_phy.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_exceptions.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_gen/vector_data.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/renesas.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_gcc.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/R7FA6M5BH.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/core_cm33.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_version.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/cmsis_compiler.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/arm/CMSIS_6/CMSIS/Core/Include/m-profile/armv8m_mpu.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/system.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/cmsis/Device/RENESAS/Include/system.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_common.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../inc/api/fsp_common_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_compiler_support.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_tfu.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_sdram.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_register_protection.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_irq.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_io.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_group_irq.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_clocks.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_module_stop.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_security.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../inc/fsp_features.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/fsp_common_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../inc/../../fsp/src/bsp/mcu/all/bsp_compiler_support.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_delay.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/../../src/bsp/mcu/all/bsp_mcu_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/instances/r_ioport.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/r_ioport_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra/fsp/inc/api/bsp_api.h:
+/home/xenwithu/e2_studio/workspace/Day1_Blinky_One_By_One_RGB_Same_/ra_cfg/fsp_cfg/r_ioport_cfg.h:
